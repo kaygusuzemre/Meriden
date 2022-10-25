fe6c23ec32b149db73d5918c51a6c7d54f7a1150
kaygusuzemre <emrekaygusuz95@gmail.com>
2022-10-25 13:25:14 +0200

    kleinEdit

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,7 +3,7 @@
         <div class="row-8 row row-cols-lg-2 row-cols-md-2 row-cols-sm-1 pl-3">
             <img src="https://images.squarespace-cdn.com/content/v1/618554f06b8283365679ba7a/1636127985112-PFMB5YC7KTBTMJM67FM3/image-asset.jpeg?format=1000w"
                 alt="" class="item">
-            <div class="d-flex flex-column item align-items-center text-center">
+            <div class="d-flex flex-column justify-content-between item align-items-center text-center">
                 <h1 class="header-2">MERIDEN COACHING</h1>
                 <div class="pb-4">
                     <p class="pb-4">
@@ -16,23 +16,30 @@
                         LEBEN
                     </p>
                 </div>
-                <input type="button" class="btn btn-outline-danger text-uppercase p-3 item m-5"
+                <div></div>
+                <input type="button" class="btn btn-outline-danger text-uppercase ps-4 pe-4 p-3"
                     value="gemeinsame arbeit">
             </div>
         </div>
-        <div class="row-4 info bg-light d-flex flex-column align-items-center text-center mt-5 p-5">
-            <img src="https://images.squarespace-cdn.com/content/v1/618554f06b8283365679ba7a/1636127985135-WSJLOEP8G5HU93BZWJPC/Star%402x.png?format=300w"
-                alt="" width="10%" height="auto">
-            <h2 class="pe-5 ps-5">Zuhören ist die Grundlage meines Coaching-Ansatzes. Ich unterstütze dich dabei,
-                wie du
-                deine Ängste
-                überwinden und genau
-                das Leben leben kannst, das du dir wünschst.</h2>
+        <div class="row-4 bg-light d-flex flex-column align-items-center text-center mt-5">
+            <div class="cardInfo p-5">
+                <img src="https://images.squarespace-cdn.com/content/v1/618554f06b8283365679ba7a/1636127985135-WSJLOEP8G5HU93BZWJPC/Star%402x.png?format=300w"
+                    alt="" width="15%" height="auto">
+                <h2 class="">Zuhören ist die Grundlage meines Coaching-Ansatzes. Ich unterstütze dich dabei,
+                    wie du
+                    deine Ängste
+                    überwinden und genau
+                    das Leben leben kannst, das du dir wünschst.</h2>
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
+.cardInfo {
+    max-width: 75%
+}
+
 p {
     font-size: 2vw;
 }
@@ -42,7 +49,7 @@ h1 {
 }
 
 h2 {
-    font-size: 2.5vw;
+    font-size: 2.3vw;
 }
 
 .btn {
